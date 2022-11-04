187a23b9a9436216b97e418b0c20030f63d629a5
Abhijith N <abhijithnairh5555@gmail.com>
2022-11-04 20:14:21 +0530

    Week 8 Assignment

---
+++ Week-8/Exercise 8.1/TreeDepth.js
@@ -0,0 +1,29 @@
+// Given tree as an array max depth will be array of length n will be n/(2i+1) => i = (n-1)/2 + 1
+class Node {
+	constructor(value) {
+		this.date = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+function maxDepth(node) {
+	if (node == null) return 0;
+	else {
+		let lDepth = maxDepth(node.left);
+		let rDepth = maxDepth(node.right);
+
+		if (lDepth > rDepth) {
+            return lDepth + 1;
+        }
+		else {
+            return rDepth + 1;
+        }
+	}
+}
+const root = new Node(3);
+root.left = new Node(9);
+root.right = new Node(20);
+root.right.left = new Node(15);
+root.right.right = new Node(7);
+
+console.log(maxDepth(root));
